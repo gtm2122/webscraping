76fb86fbc998ff845889398c549cf55d76626b15
gtm2122 <gtm2122@github.com>
2016-09-12 15:40:48 -0700

    returns products

--- swused.py
+++ swused.py
@@ -5,9 +5,8 @@ import requests
 def get_products(company):
  response = requests.post('https://siftery.com/company/'+company)
  data = response.json()
- #companies = data['content']['companies']
 
- print (data['response']['products'].keys()[1])
+ return data['response']['products'].keys()
 
 
 
